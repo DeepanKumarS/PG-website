dbd3d0e2b17bec92e8f24862a58f97abdaacb800
Deepan <deepankumar2212@gmail.com>
2026-07-11 13:34:02 +0530

    spacing issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
         <NavLink to="/" className="flex items-center gap-2">
           <img src="/letter-l.png" alt="Loyola PG" className="h-10 w-auto" />
-          <span className=" px-3 py-1 font-semibold text-black">Loyola PG</span>
+          <span className="font-semibold text-black">Loyola PG</span>
         </NavLink>
 
         <nav className="flex items-center gap-1">
